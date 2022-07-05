bc6d0473f4e0a4f842f91e5845003fe2604fa602
AmyRom <1015403@hr.nl>
2022-07-05 12:55:09 +0200

    Docs: Local storage add or delete
    functie om objecten toe te voegen aan of te verwijderen uit de local storage.

--- js/main.js
+++ js/main.js
@@ -42,4 +42,22 @@ function getFavCars(){
     }
 }
 
-
+//(favButton onclick) add or remove a car to favCars array
+function addOrRemoveFav(carsID){
+    //if car is already in the favCars array
+    let carName = carsDetails[carsID].name;
+    if(favCars.includes(carName)){
+        //find the position of the car in favCars array
+        let pos = favCars.indexOf(carName)
+        //remove car from the array
+        favCars.splice(pos, 1);
+    } else {
+        //else (if car is not in favCars array)
+        //add car to favCars array
+        favCars.push(carName);
+    }
+    //stringify favCars array and set it in local Storage
+    localStorage.setItem('myFavoriteCars', JSON.stringify(favCars));
+    //force reload of page
+    location.reload()
+}
